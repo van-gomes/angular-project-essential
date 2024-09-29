3deafe783db55888149250baf3837162c7a2418f
van-gomes <nninha.gomes@gmail.com>
2024-09-29 20:43:35 -0300

    Encode header

--- products-frontend/src/app/app.component.ts
+++ products-frontend/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/templats/header/header.component';
+
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: 
+  [ RouterOutlet, 
+    HeaderComponent,
+  ],
   templateUrl: 'app.component.html'
 })
 export class AppComponent {}
