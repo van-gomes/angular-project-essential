f7675efff989cadc1e36df9bb9bea644c32037dd
van-gomes <nninha.gomes@gmail.com>
2024-10-15 16:14:09 -0300

    Create product-update component and configure its route

--- products-frontend/src/app/app.routes.ts
+++ products-frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from "./views/home/home.component";
 import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 export const routes: Routes = [
     {
@@ -17,4 +18,8 @@ export const routes: Routes = [
         path: "products/create",
         component: ProductCreateComponent
       },
+      {
+        path: "products/update/:id",
+        component: ProductUpdateComponent
+      }
 ];
