0e48d2b66e9f6bde241ecd2d34e5a959a2a2cb7d
van-gomes <nninha.gomes@gmail.com>
2024-10-18 18:59:39 -0300

    Create Component Delete

---
+++ products-frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-delete',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-delete.component.html',
+  styleUrl: './product-delete.component.css'
+})
+export class ProductDeleteComponent {
+
+}
