656f4c7fc859264a85eb892e9c68cc2d6103b46c
van-gomes <nninha.gomes@gmail.com>
2024-10-19 17:09:04 -0300

    Make logic and css Component Delete

--- products-frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ products-frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-product-delete',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule
+  ],
   templateUrl: './product-delete.component.html',
   styleUrl: './product-delete.component.css'
 })
 export class ProductDeleteComponent {
+  product: Product = { id: 0, name: '', price: 0 };
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = +(this.route.snapshot.paramMap.get("id") ?? 0);
+    this.productService.readById(id).subscribe((product) => {
+      this.product = product;
+    });
+  }
+
+  deleteProduct(): void {
+    if (this.product.id !== undefined) {
+      this.productService.delete(this.product.id).subscribe(() => {
+        this.productService.showMessage("Produto excluido com sucesso!");
+        this.router.navigate(["/products"]);
+      });
+    }
+  }
+
+  cancel(): void {
+    this.router.navigate(["/products"]);
+  }
 
 }
--- products-frontend/src/app/views/product-crud/product-crud.component.ts
+++ products-frontend/src/app/views/product-crud/product-crud.component.ts
@@ -3,13 +3,15 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductReadComponent } from '../../components/product/product-read/product-read.component';
 import { ProductUpdateComponent } from '../../components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from '../../components/product/product-delete/product-delete.component';
 
 @Component({
   selector: 'app-product-crud',
   standalone: true,
   imports: [
     ProductReadComponent,
-    ProductUpdateComponent
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   templateUrl: './product-crud.component.html',
   styleUrl: './product-crud.component.css'
