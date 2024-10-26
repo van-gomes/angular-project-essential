65993a178470d93f91ef1c5b9b22f324f131d0f0
van-gomes <nninha.gomes@gmail.com>
2024-10-26 17:02:25 -0300

    Transfer Save Button to ProductsRead Component

--- products-frontend/src/app/components/product/product-read/product-read.component.ts
+++ products-frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,11 +1,12 @@
 import { Component, LOCALE_ID } from '@angular/core';
 import { NgFor, CommonModule } from '@angular/common';
 import { Product } from '../product.model';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { ProductService} from '../product.service';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from  '@angular/common';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
 registerLocaleData(localePt);
 
@@ -16,6 +17,7 @@ registerLocaleData(localePt);
     NgFor,
     CommonModule,
     MatTableModule,
+    MatButtonModule,
     RouterModule
   ],
   providers: [{
@@ -29,11 +31,15 @@ export class ProductReadComponent {
   products: Product[] = [];
   displayedColumns = ['id', 'name', 'price', 'action']
   
-  constructor(private productService: ProductService) { }
+  constructor(private router: Router, private productService: ProductService) { }
 
   ngOnInit(): void {
     this.productService.read().subscribe(products => {
       this.products = products
     })
+  }
+
+  navigateToProductCreate(): void {
+    this.router.navigate(['/products/create'])
   }
 }
--- products-frontend/src/app/components/templats/header/header.component.ts
+++ products-frontend/src/app/components/templats/header/header.component.ts
@@ -4,7 +4,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderService } from './header.service';
 import { HomeComponent } from '../../../views/home/home.component';
 import { ProductReadComponent } from '../../product/product-read/product-read.component';
-import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
